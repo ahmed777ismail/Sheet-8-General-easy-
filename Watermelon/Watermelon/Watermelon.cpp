1510ddd30f68e5d6e51fe89afffe87bd1d09af2d
ahmed ismail <87954455+ahmed777ismail@users.noreply.github.com>
2024-11-01 07:15:18 +0200

    Add files via upload
    Watermelon

---
+++ Watermelon/Watermelon/Watermelon.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() //
+{
+	int size;
+	cin >> size;
+	if (size <= 2) {
+		cout << "NO";
+		return 0;
+	}
+	if (size % 2 == 0) {
+		cout << "YES";
+	}
+	else {
+		cout << "NO";
+	}
+}
